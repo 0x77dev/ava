d90aae54a356f8ca0193c530d8b26f9b7bfe15e4
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-04-19 23:39:50 -0700

    Fri Apr 19 23:39:50 PDT 2024

--- skills/homeassistant/provider.ts
+++ skills/homeassistant/provider.ts
@@ -1,13 +1,27 @@
-import { Auth, createConnection } from "home-assistant-js-websocket";
+import { Auth, createConnection, createLongLivedTokenAuth } from "home-assistant-js-websocket";
 import { HOMEASSISTANT } from "./config";
 
+const auth = createLongLivedTokenAuth(
+  HOMEASSISTANT.url,
+  HOMEASSISTANT.token
+);
+
 export const hass = await createConnection({
-  auth: new Auth({
-    access_token: HOMEASSISTANT.token,
-    hassUrl: HOMEASSISTANT.url,
-    clientId: '',
-    expires: Infinity,
-    expires_in: Infinity,
-    refresh_token: '',
-  })
+  auth,
+  createSocket(options) {
+    if (options.auth && new URL(HOMEASSISTANT.url).hostname === "supervisor") {
+      options.auth = {
+        data: {
+          ...options.auth.data,
+          hassUrl: "http://supervisor/core"
+        },
+        wsUrl: "ws://supervisor/core/websocket",
+        accessToken: HOMEASSISTANT.token,
+        async refreshAccessToken() {},
+        expired: false,
+        async revoke() {}
+      }
+    }
+    return options.createSocket(options)
+  },
 });
