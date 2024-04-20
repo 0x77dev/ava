c2f46d50d6397da4d6ccb759110a9306abc7df99
Mykhailo Marynenko <mykhailo@0x77.dev>
2024-04-19 18:56:37 -0700

    fix server addon setup

--- server/hass-entrypoint.sh
+++ server/hass-entrypoint.sh
@@ -3,3 +3,5 @@
 LLM="$(bashio::config 'llm' | jq -r .llm)"
 EMBEDDINGS="$(bashio::config 'embeddings' | jq -r .llm)"
 HOMEASSISTANT='{"url": "ws://supervisor/core/websocket", "token": "${SUPERVISOR_TOKEN}"}'
+
+bun run start
